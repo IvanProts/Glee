69357c286a84ad9d0c39b6044125a12781418cba
Проць Иван <ivanprotss@gmail.com>
2021-03-25 21:27:53 +0200

    made optimization, bug fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,133 @@
-const {src, dest, watch, parallel, series} = require('gulp');
-const scss          = require('gulp-sass');
-const concat        = require('gulp-concat');
-const autoprefixer  = require('gulp-autoprefixer');
-const uglify        = require('gulp-uglify');
-const imagemin      = require('gulp-imagemin');
-const del           = require('del');
-const browserSync   = require('browser-sync').create();
+var gulp = require('gulp'),
+  svgSprite = require('gulp-svg-sprite');
 
-function browsersync(){
+var config = {
+  shape: {
+    dimension: { // Set maximum dimensions
+      maxWidth: 500,
+      maxHeight: 500
+    },
+    spacing: { // Add padding
+      padding: 0
+    }
+  },
+  mode: {
+    view: { // Activate the «view» mode
+      bust: false,
+      render: {
+        scss: true // Activate Sass output (with default options)
+      }
+    },
+    symbol: {
+      dest: '.'
+    } // Activate the «symbol» mode
+  }
+};
+
+gulp.task('svg-sprite', function (cb) {
+  return gulp.src('app/images/icons/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('app/images/icons/sprite'));
+});
+
+
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
   browserSync.init({
     server: {
       baseDir: 'app/'
     },
-    notify:false
+    notify: false
   })
 }
 
 
 function styles() {
   return src('app/scss/style.scss')
-  .pipe(scss({outputStyle: 'compressed'}))
-  .pipe(concat('style.min.css'))
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 10 versions'],
-    grid:true
-  }))
-  .pipe(dest('app/css'))
-  .pipe(browserSync.stream())
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
-function scripts(){
+function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/mixitup/dist/mixitup.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
-function images(){
+function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
         ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
+      })
+    ]))
+    .pipe(dest('dist/images'))
 }
 
-function build(){
+function build() {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base:'app'})
-  .pipe(dest('dist'))
+      'app/**/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'))
 }
 
-function cleanDist(){
+function cleanDist() {
   return del('dist')
 }
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
-  watch(['app/**/*.html']).on('change',browserSync.reload);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
