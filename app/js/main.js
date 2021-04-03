e9a0eb0390ef28e33759c60e611ade2b5575febf
Проць Іван <ivanprotss@gmail.com>
2021-04-03 19:29:34 +0300

    made adaptive for home page

--- app/js/main.js
+++ app/js/main.js
@@ -4,26 +4,51 @@ $(function () {
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000
+    // autoplay: true,
+    // autoplaySpeed: 2000
   });
 
   $('.partners__slider').slick({
     arrows: false,
     slidesToShow: 5,
+    infinite: true,
+    slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    responsive: [{
+      breakpoint: 968,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 440,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
   });
+
+  $('.user-menu__items').on('click', function () {
+    $('.menu').toggleClass('menu--active');
+  });
+
   var product1 = document.querySelector('[data-ref="product"]');
   var design = document.querySelector('[data-ref="design"]');
 
   var config = {
     controls: {
-    scope: "local",
-  },
-};
+      scope: "local",
+    },
+  };
 
-var mixer = mixitup(product1, config);
-var mixer2 = mixitup(design, config);
+  var mixer = mixitup(product1, config);
+  var mixer2 = mixitup(design, config);
 
 });
